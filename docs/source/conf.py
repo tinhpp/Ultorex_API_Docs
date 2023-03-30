ddf0739cc8106d2603063c9504f3b8a0549f3832
tinh.phan <tinh.phan@napaglobal.com>
2023-03-30 17:02:29 +0700

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.osexample',
+    'sphinx.ext.autosectionlabel',
+    'sphinxcontrib.osexample'
 ]
 
 intersphinx_mapping = {
